98a7d33882378c57c19ce95b5d1b7ef51c5c5394
viniciusdacal <viniciusldacal@gmail.com>
2015-08-08 14:04:49 -0300

    removed from templates

--- app/templates/e2e/_main.po.js
+++ app/templates/e2e/_main.po.js
@@ -6,11 +6,7 @@
 'use strict';
 
 var MainPage = function() {
-<% if (props.ui.key === 'foundation') { -%>
-  this.jumbEl = element(by.css('.panel'));
-<% } else { -%>
   this.jumbEl = element(by.css('.jumbotron'));
-<% } -%>
   this.h1El = this.jumbEl.element(by.css('h1'));
   this.imgEl = this.jumbEl.element(by.css('img'));
   this.thumbnailEls = element(by.css('body')).all(by.repeater('awesomeThing in main.awesomeThings'));
